c5911c69701342afb50459333d4bfece955417d4
NguyennQuoccKhanhh <quockhanhh129@gmail.com>
2024-10-31 17:37:26 +0700

    khanhcodengay31/10

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
+
+
+const topMenu = document.getElementById('custom-menu')
+const topMenuIcon = document.getElementById('toggle-custom-menu')
+
+document.addEventListener('click', (e) => {
+    if(topMenuIcon.contains(e.target)){
+        topMenu.classList.toggle('hidden');
+    } else if(!topMenu.contains(e.target)){
+        topMenu.classList.add('hidden');
+    }
+})
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,     
     grid: {
         rows: 3,              
+    },
+    navigation: {
+        nextEl: '.docterNext',
+        prevEl: '.docterPrev',
     },
            
     pagination: {
